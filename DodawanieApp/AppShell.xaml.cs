c03495b97512c059799ccab3a63ee5fed27a2d49
Marcin Wojciechowski <marcin.wojciechowski@goleniow.edu.pl>
2024-11-14 10:02:47 +0100

    Dodaj pliki projektów.

---
+++ DodawanieApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DodawanieApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DodawanieApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace DodawanieApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var suma = Convert.ToDouble(PierwszaLiczba.Text) + Convert.ToDouble(DrugaLiczba.Text);
+            Wynik.Text = "Wynik: " + suma;
+        }
+    }
+
+}
